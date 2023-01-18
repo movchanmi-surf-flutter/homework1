307dd6e2623206b8f7a082c4eb548347a030691f
GhotuSs <maksmovchan@icloud.com>
2023-01-18 22:06:40 +0300

    'appbar'

--- lib/ui/screen/sight_list_screen.dart
+++ lib/ui/screen/sight_list_screen.dart
@@ -8,9 +8,31 @@ class SightListScreen extends StatefulWidget {
 }
 
 class _SightListScreenState extends State<SightListScreen> {
+  TextStyle _textStyle({required Color textColor})=>TextStyle(
+    fontFamily: 'Roboto',
+    fontSize: 32,
+    fontWeight: FontWeight.w700,
+    color: textColor,
+  );
   @override
-  Widget build(BuildContext context)=>const Scaffold(
+  Widget build(BuildContext context)=>Scaffold(
+    backgroundColor: Colors.white,
     // resizeToAvoidBottomInset: false,
+    appBar: AppBar(
+      backgroundColor: Colors.white,
+      shadowColor: Colors.transparent,
+      centerTitle: false,
+      toolbarHeight: 72,
+      title: RichText(text: TextSpan(
+        text: 'C',
+        style: _textStyle(textColor: const Color(0xFF4CAF50)),
+        children: [
+          TextSpan(text: 'писок\n',style: _textStyle(textColor: const Color(0xFF252849),),),
+          TextSpan(text: 'и',style: _textStyle(textColor: const Color(0xFFFBC02D),),),
+          TextSpan(text: 'нтересных мест',style: _textStyle(textColor: const Color(0xFF252849),),),
+        ]
+      ),maxLines: 2,),
+    ),
     body: Center(
       child: Text('Hello!'),
     ),
