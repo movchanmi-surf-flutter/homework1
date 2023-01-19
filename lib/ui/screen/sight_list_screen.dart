1be765431b946598bb431d3c2a4bfdf52c09585a
GhotuSs <maksmovchan@icloud.com>
2023-01-19 12:47:36 +0300

    'Features: sightCard'

--- lib/ui/screen/sight_list_screen.dart
+++ lib/ui/screen/sight_list_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:places/mocks.dart';
+import 'package:places/ui/screen/sight_card.dart';
 
 class SightListScreen extends StatefulWidget {
   const SightListScreen({Key? key}) : super(key: key);
@@ -8,33 +10,35 @@ class SightListScreen extends StatefulWidget {
 }
 
 class _SightListScreenState extends State<SightListScreen> {
-  TextStyle _textStyle({required Color textColor})=>TextStyle(
-    fontFamily: 'Roboto',
-    fontSize: 32,
-    fontWeight: FontWeight.w700,
-    color: textColor,
-  );
+  TextStyle _textStyle({required Color textColor}) => TextStyle(
+        fontFamily: 'Roboto',
+        fontSize: 32,
+        fontWeight: FontWeight.w700,
+        color: textColor,
+      );
   @override
-  Widget build(BuildContext context)=>Scaffold(
-    backgroundColor: Colors.white,
-    // resizeToAvoidBottomInset: false,
-    appBar: AppBar(
-      backgroundColor: Colors.white,
-      shadowColor: Colors.transparent,
-      centerTitle: false,
-      toolbarHeight: 72,
-      title: RichText(text: TextSpan(
-        text: 'C',
-        style: _textStyle(textColor: const Color(0xFF4CAF50)),
-        children: [
-          TextSpan(text: 'писок\n',style: _textStyle(textColor: const Color(0xFF252849),),),
-          TextSpan(text: 'и',style: _textStyle(textColor: const Color(0xFFFBC02D),),),
-          TextSpan(text: 'нтересных мест',style: _textStyle(textColor: const Color(0xFF252849),),),
-        ]
-      ),maxLines: 2,),
-    ),
-    body: Center(
-      child: Text('Hello!'),
-    ),
-  );
+  Widget build(BuildContext context) => Scaffold(
+        backgroundColor: Colors.white,
+        // resizeToAvoidBottomInset: false,
+        appBar: AppBar(
+          backgroundColor: Colors.white,
+          shadowColor: Colors.transparent,
+          centerTitle: false,
+          toolbarHeight: 72,
+          title: Text(
+            'Список\nинтересных мест',
+            style: _textStyle(
+              textColor: const Color(0xFF252849),
+            ),
+          ),
+        ),
+        body: SafeArea(
+          bottom: false,
+          minimum: const EdgeInsets.only(left: 16,right: 16,top: 16),
+          child: ListView.separated(
+              itemCount: mocks.length,
+              itemBuilder:(_,index)=>SightCard(sight: mocks[index]),
+            separatorBuilder: (_,index)=>const SizedBox(height: 16,),),
+        ),
+      );
 }
