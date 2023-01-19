499d7980ca3efe5db1fcba2b5696ac34a2e14887
GhotuSs <maksmovchan@icloud.com>
2023-01-19 11:58:48 +0300

    'Features: dataModel + mocks'

---
+++ lib/domain/sight.dart
@@ -0,0 +1,17 @@
+class Sight {
+  Sight({
+    required this.name,
+    required this.lat,
+    required this.lon,
+    required this.details,
+    required this.type,
+    required this.url,
+});
+
+  String name;
+  double lat;
+  double lon;
+  String url;
+  String details;
+  String type;
+}
---
+++ lib/mocks.dart
@@ -0,0 +1,29 @@
+import 'package:places/domain/sight.dart';
+
+final List<Sight> mocks = [
+  Sight(
+    name: "Линдулловская роща",
+    lat: 60.237512,
+    lon: 29.539790,
+    details: "Обороудованная экотропа, засаженная по истине огромными лиственницами 18 века.",
+    type: "Природа",
+    url: "https://pp.userapi.com/c840629/v840629733/10d1e/HBnWCpZ0dj4.jpg",
+  ),
+  Sight(
+    name: "Гастропаб Neighbours",
+    lat: 59.869237,
+    lon: 30.459877,
+    details: "Уютный гастропаб с дружественной атмосферой и демократичными ценами",
+    type: "Ресторан",
+    url: "https://avatars.mds.yandex.net/get-altay/235931/2a0000015d7a05220fe4acc11327c89c5c46/XXL",
+  ),
+  Sight(
+    name: "Пляж у Осиновецкого маяка",
+    lat: 60.119137,
+    lon: 31.082225,
+    details: "Оборудованный пляж в Ленинградской области, расположенный у Ладожского озера.",
+    type: "Природа",
+    url: "https://walkday.ru/upload/iblock/04d/04df49979bbc0fa10cf40b2c5636daee.jpg",
+  ),
+];
+
